ef6214d5662f902355147a960da66ee34a4d97a0
Arthur Benjamin Domokos <domokosbeni@yahoo.com>
2016-04-29 08:55:25 +0100

    commit

--- app/src/main/java/com/example/benjamindomokos/landlordtenant/DisplayComplexMessages.java
+++ app/src/main/java/com/example/benjamindomokos/landlordtenant/DisplayComplexMessages.java
@@ -24,6 +24,7 @@ import java.util.Map;
 public class DisplayComplexMessages extends AppCompatActivity {
 
     ListView messageListView;
+
     List<ComplexMessage> messageList = new ArrayList<ComplexMessage>();
 
     @Override
@@ -33,7 +34,9 @@ public class DisplayComplexMessages extends AppCompatActivity {
 
         Bundle extras = getIntent().getExtras();
 
+
         TextView messageType = (TextView) findViewById(R.id.textComplexType);
+        messageListView = (ListView) findViewById(R.id.listView4);
         if (extras.getInt("messageType") == 1)
             messageType.setText("Maintenance Requests");
         else if (extras.getInt("messageType") == 2)
@@ -42,6 +45,39 @@ public class DisplayComplexMessages extends AppCompatActivity {
             messageType.setText("Reminders");
 
 
+        Response.Listener<String> responseListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                JSONArray array = new JSONArray();
+                try {
+                    JSONObject json = new JSONObject(response);
+                    array = json.getJSONArray("value");
+                    for (int i = 0; i < array.length(); i++) {
+                        JSONObject user = array.getJSONObject(i);
+                        String subject = user.getString("subject");
+                        String time = user.getString("time");
+                        String message = user.getString("message");
+
+                        messageList.add(new ComplexMessage(subject,time,message));
+                        // Log.d("added", firstname + lastname + username);
+
+                    }
+                    ComplexMessageListAdapter adapter = new ComplexMessageListAdapter();
+                    messageListView.setAdapter(adapter);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        Map<String ,String > parameters = new HashMap<String, String>();
+
+        parameters.put("group", extras.getInt("group")+"");
+        parameters.put("type", extras.getInt("messageType")+"");
+
+        CustomStringRequest request = new CustomStringRequest(parameters, "http://openexport.esy.es/getcomplex.php", responseListener);
+        RequestQueue queue = Volley.newRequestQueue(DisplayComplexMessages.this);
+        queue.add(request);
 
     }
 
@@ -55,8 +91,8 @@ public class DisplayComplexMessages extends AppCompatActivity {
         if (view == null)
             view = getLayoutInflater().inflate(R.layout.complex_message_layout, parent, false);
         ComplexMessage user = messageList.get(position);
-        TextView name = (TextView) view.findViewById(R.id.textMessage);
-        name.setText(user.getMessage());
+        TextView message = (TextView) view.findViewById(R.id.textMessage);
+        message.setText(user.getMessage());
 
         TextView subject = (TextView) view.findViewById(R.id.textSubject);
         subject.setText(user.getSubject());
